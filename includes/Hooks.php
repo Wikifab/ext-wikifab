98a0a792b6aee458ea6d5842610b2c20bba1f2dd
Pierre Boutet <boutetpierre@yahoo.fr>
2019-11-06 15:40:54 +0100

    init $wgInitPages param

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -4,6 +4,12 @@ namespace Wikifab;
 class Hooks {
 
 	public static function onRegistration() {
+		global $wgInitPages;
+
+		if ( ! isset($wgInitPages) ) {
+			$wgInitPages = [];
+		}
+		$wgInitPages[] = dirname( __DIR__) . "/initPages/dokit-base";
 	}
 
 	public static function initialize() {
